cb539c113a67643e2c79800e2b23f5d9a2503ed8
Moses Holmström <thykka@gmail.com>
2023-10-22 20:51:16 +0300

    markdown support

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
 "use strict";
+import config from './config.json';
+import articles from './articles/index.json';
+import marked from 'marked';
+
+// document.body.innerHTML = marked.parse(hw);
+
+console.log(articles);
+
+const loadArticle = async (article) => {
+  const articlePath = './articles/01-hello-world.md'; //config.articlesPath + article.file;
+  console.log(articlePath);
+  let articleContent = await import(articlePath);
+  console.log(articleContent);
+};
+
+loadArticle(articles[0]);
